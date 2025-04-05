e095315ac5b8326df9448c291e63b5cf0ad3cd29
Gustavo <ovatleyva@gmail.com>
2025-04-04 17:17:09 -0700

    Initial commit: Astro project setup - c4cydonia website

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  site: 'https://c4cydonia.com',
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
